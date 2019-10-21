487aa71a0073822bf599a6b778483d08d98fa024
Thulio josé Mariano <thulio.mariano@gmail.com>
2019-10-21 12:53:25 -0200

    Alterado o padrão MVC e criado Enum

---
+++ Registro de RMA/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_de_RMA.Entities.Enum
+{
+    enum OrderStatus
+    {
+        Aberto,
+        Andamento,
+        Pausa,
+        Cancelado,
+        Fechado
+    }
+}
