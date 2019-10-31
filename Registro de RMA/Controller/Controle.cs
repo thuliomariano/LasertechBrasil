222c692536c5eb17dff81f3b923e278a9a79867a
Thulio josé Mariano <thulio.mariano@gmail.com>
2019-10-31 17:57:13 -0300

    Inserido método de cadastroArm com validação

--- Registro de RMA/Controller/Controle.cs
+++ Registro de RMA/Controller/Controle.cs
@@ -84,5 +84,22 @@ namespace Registro_de_RMA.Modelo
 
             return Mensagem;
         }
+
+        public String CadastrarArm(Arm arm)
+        {
+
+            ValidaArm validaArm = new ValidaArm(arm);
+            if (validaArm.Mensagem.Equals(""))
+            {
+                ArmDao armDao = new ArmDao();
+                return Mensagem = armDao.cadastrarArm(arm);
+            }
+            else
+            {
+                Mensagem = validaArm.Mensagem;
+            }
+
+            return Mensagem;
+        }
     }
 }
