6a50097fa6172a9c94f3762a83f248b0e5a1415a
Thulio josé Mariano <thulio.mariano@gmail.com>
2019-11-05 12:40:07 -0300

    Validação para os dados de entrada da câmera

--- Registro de RMA/Views/CadastrarCamera.cs
+++ Registro de RMA/Views/CadastrarCamera.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Registro_de_RMA.Entities;
+﻿using Registro_de_RMA.Entities;
 using Registro_de_RMA.Modelo;
-using Registro_de_RMA.Entities.Enum;
+using System;
+using System.Windows.Forms;
 
 namespace Registro_de_RMA.Views
 {
@@ -23,22 +15,74 @@ namespace Registro_de_RMA.Views
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
             Camera camera = new Camera();
-            camera.Fornecedor = txtFornecedor.Text.ToUpper();
-            camera.Observacao = txtObservacaoCamera.Text.ToUpper();
-            camera.Mac = txtMac.Text.ToUpper();
-            camera.CameraDataDeEntrada =  DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss");
-            camera.CameraDataDeSaida = DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss");
-            camera.Acessorio = txtAcessório.Text.ToUpper();
-            camera.CameraPuma = txtProduto.Text.ToUpper();
-            camera.CameraStatus = "ABERTO";
-            Controle controle = new Controle();
-            controle.CadastrarCamera(camera);
-            MessageBox.Show(controle.Mensagem, "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
-           
+            try
+            {
+                if (txtFornecedor.Text == "")
+                {
+                    camera.Fornecedor = "";
+                }
+                else
+                {
+                    camera.Fornecedor = txtFornecedor.Text.ToUpper();
+                }
+                if (txtObservacaoCamera.Text == "")
+                {
+                    camera.Observacao = "";
+                }
+                else
+                {
+                    camera.Observacao = txtObservacaoCamera.Text.ToUpper();
+                }
+                if (txtMac.Text == "")
+                {
+                    camera.Mac = "";
+                }
+                else
+                {
+                    camera.Mac = txtMac.Text.ToUpper();
+                }
+                if (txtAcessório.Text == "")
+                {
+                    camera.Acessorio = "";
+                }
+                else
+                {
+                    camera.Acessorio = txtAcessório.Text.ToUpper();
+                }
+                if (txtProduto.Text == "")
+                {
+                    camera.CameraPuma = "";
+                }
+                else
+                {
+                    camera.CameraPuma = txtProduto.Text.ToUpper();
+                }
+                camera.CameraDataDeEntrada = DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss");
+                camera.CameraDataDeSaida = DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss");
+                camera.CameraStatus = "ABERTO";
+            }
+            catch (Exception erroDeDados)
+            {
+
+                MessageBox.Show(erroDeDados.ToString());
+            }
+
+            try
+            {
+                Controle controle = new Controle();
+                controle.CadastrarCamera(camera);
+                MessageBox.Show(controle.Mensagem, "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception PassagemDeDados)
+            {
+                MessageBox.Show(PassagemDeDados.ToString());
+            }
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
+            try
+            {
             txtAcessório.Text = "";
             txtApontamento.Text = "";
             txtFornecedor.Text = "";
@@ -46,6 +90,13 @@ namespace Registro_de_RMA.Views
             txtObservacaoCamera.Text = "";
             txtPatrimonioCamera.Text = "";
             txtProduto.Text = "";
+
+            }
+            catch (Exception capturarDadosCamera)
+            {
+
+                MessageBox.Show(capturarDadosCamera.ToString());
+            }
         }
 
         private void CadastrarCamera_Load(object sender, EventArgs e)
